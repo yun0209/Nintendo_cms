a2df13b7d369bbbeaa5ed424d1c469624f64ba3c
tree4852 <tree4852@naver.com>
2024-08-14 17:55:38 +0900

    헤더 반응형 진행중 & 그누보드 페이지연결

--- top.php
+++ top.php
@@ -1,6 +1,7 @@
 <?php 
 include_once("./_common.php"); 
 include_once(G5_LIB_PATH.'/latest.lib.php');
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,6 +24,7 @@ include_once(G5_LIB_PATH.'/latest.lib.php');
     <link rel="stylesheet" href="<?php echo  G5_JS_URL; ?>/font-awesome/css/font-awesome.min.css?ver=2303229">
     <link rel="stylesheet" href="<?php echo  G5_URL; ?>/skin/board/basic/style.css?ver=2303229">
     <link rel="stylesheet" href="<?php echo  G5_URL; ?>/skin/faq/basic/style.css?ver=2303229">
+    <link rel="stylesheet" href="<?php echo  G5_URL; ?>/skin/search/basic/style.css?ver=2303229">
     <link rel="stylesheet" href="<?php echo  G5_THEME_CSS_URL; ?>/default.css">
        
     <!-- 커스터마이징 -->
@@ -45,7 +47,7 @@ include_once(G5_LIB_PATH.'/latest.lib.php');
                 </a>
             </h1>
             <div class="col">
-                <ul class="gnb d-none  jsb d-lg-flex aic col-8 mx-auto">
+                <ul class="gnb d-none  jsb d-lg-flex aic col-8 ms-auto">
                     <li class="gnb_li"><a href="" class="gnbT fontBase">본체 및 amlibo</a></li>
                     <li class="gnb_li"><a href="" class="gnbT fontBase">소프트웨어</a></li>
                     <li class="gnb_li"><a href="/nintendo_cms/bbs/faq.php?fm_id=1" class="gnbT fontBase">고객지원</a></li>
@@ -53,12 +55,63 @@ include_once(G5_LIB_PATH.'/latest.lib.php');
                 </ul>
                  
             </div>
-            <div class="d-flex myicon">
-                <ul class="d-flex ">
-                    <li class="me-3 d-none d-lg-block"><a href=""><img src="/img/svg/search_icon.svg" alt="검색"></a></li>
-                    <li class="d-lg-none"><a href=""><img src="/img/svg/menu_bar.svg" alt="메뉴펼침"></a></li>
-                    <li class="d-none d-lg-block"><a href=""><img src="/img/svg/globarl.svg" alt="번역"></a></li>
-                </ul> 
+            
+            <div class="d-none d-lg-flex myicon me-5">
+                <div class="d-flex ">
+                    <fieldset class="hd_sch">
+                        <legend>사이트 내 전체검색</legend>
+                        <form name="fsearchbox" method="get" action="http://yysportfolio.dothome.co.kr/nintendo_cms/bbs/search.php" onsubmit="return fsearchbox_submit(this);">
+                            <input type="hidden" name="sfl" value="wr_subject||wr_content">
+                            <input type="hidden" name="sop" value="and">
+                            <label for="sch_stx" class="sound_only">검색어 필수</label>
+                            <input type="text" name="stx" class="sch_stx" maxlength="20" placeholder="검색어를 입력해주세요">
+                            <button type="submit" class="sch_submit" value="검색">
+                                <img src="/img/svg/search_icon.svg" alt="">
+                                <span class="sound_only">검색</span>
+                            </button>
+                        </form>
+
+                        <script>
+                        function fsearchbox_submit(f)
+                        {
+                            var stx = f.stx.value.trim();
+                            if (stx.length < 2) {
+                                alert("검색어는 두글자 이상 입력하십시오.");
+                                f.stx.select();
+                                f.stx.focus();
+                                return false;
+                            }
+
+                            // 검색에 많은 부하가 걸리는 경우 이 주석을 제거하세요.
+                            var cnt = 0;
+                            for (var i = 0; i < stx.length; i++) {
+                                if (stx.charAt(i) == ' ')
+                                    cnt++;
+                            }
+
+                            if (cnt > 1) {
+                                alert("빠른 검색을 위하여 검색어에 공백은 한개만 입력할 수 있습니다.");
+                                f.stx.select();
+                                f.stx.focus();
+                                return false;
+                            }
+                            f.stx.value = stx;
+
+                            return true;
+                        }
+                        </script>
+
+                    </fieldset>
+                    <button class="d-lg-none"><a href=""><img src="/img/svg/menu_bar.svg" alt="메뉴펼침"></a></button>
+                    
+                    </div> 
+            </div>
+            <div class="singup fontNoto ">
+                <ul class="d-flex text-wrap">
+                    <li class = "me-3"><a href="/nintendo_cms/bbs/login.php">로그인</a></li>
+                    <li><a href="">회원가입</a></li>
+                </ul>
+
             </div>
         </div>
     </header>
